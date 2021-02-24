91c93e128f2cb146b3ddd6c87f8371dde5660046
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-02-24 23:17:54 +0200

    implemented ability add item to state from input

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
 import './item-add-form.css';
 
-export default class ItemAddForm extends Component  {
+export default class ItemAddForm extends Component {
+    state = {
+      label: ''
+    }
 
-    render () {
+    onLabelChange = (e) => {
+      this.setState({
+          label: e.target.value
+      }); 
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state.label);
+        this.setState(
+            {
+                label: ''
+            }
+        )
+    }
+    
+    render() {
+      
         const { addItem } = this.props;
-      return  (
-          <div className="item-add-form">
-              <button className="btn btn-outline-secondary" onClick={() => addItem('Hello world')}>
-                  Add Item
+        return (
+            <form 
+              className="item-add-form d-flex"
+              onSubmit={this.onSubmit}  
+            >
+                <input
+                    type="text"
+                    value={this.state.label}
+                    className="form-control item-add-form-input"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                />
+                <button className="btn btn-outline-secondary">
+                    Add Item
               </button>
-          </div>
-      )
+            </form>
+        )
     }
 }
